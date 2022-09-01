ffdefb1b484bf1baddae957bc3dcef2f1ee71e33
WenyiLULU <luwenyi1991@gmail.com>
2022-09-01 16:01:44 +0200

    component Layout

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            Copyright 2022 Next.js Training
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,11 @@
+const Ninjas = () => {
+    return ( 
+        <div>
+            <h1>
+                Here is Ninjas Home
+            </h1>
+        </div>
+     );
+}
+ 
+export default Ninjas;
